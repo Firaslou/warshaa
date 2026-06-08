270592b593eb820d291dbdfebfb38966e8e4e3e4
mayssaderbeel-png <mayssaderbeel@gmail.com>
2026-06-08 02:15:13 +0100

    Update CreatorDashboard.tsx

--- src/pages/dashboard/CreatorDashboard.tsx
+++ src/pages/dashboard/CreatorDashboard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useRef } from "react";
 import { Navigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import {
